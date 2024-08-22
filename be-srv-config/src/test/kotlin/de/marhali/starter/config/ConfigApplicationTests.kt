bb9c4599854b9b724761bf92894d1a861a99181f
marhali <me@marhali.de>
2024-08-22 16:12:01 +0200

    feat(services): config service base

---
+++ be-srv-config/src/test/kotlin/de/marhali/starter/config/ConfigApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.marhali.starter.config
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
