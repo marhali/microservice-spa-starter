8b009615b25c6fc95e515fb7c9766b2195467565
marhali <me@marhali.de>
2024-08-26 20:19:43 +0200

    feat(spa): configure linter and add tailwind

--- fe-app-spa/vite.config.ts
+++ fe-app-spa/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// <reference type="vitest" />
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsconfigPaths(), react()],
+});
