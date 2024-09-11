9bd17dd8b8e92cb6b67c2d047c7bded39f2a32f7
marhali <me@marhali.de>
2024-09-11 14:01:08 +0200

    feat(deps): add discovery client

--- be-srv-auth/build.gradle.kts
+++ be-srv-auth/build.gradle.kts
@@ -6,10 +6,11 @@ plugins {
 
 dependencies {
 	implementation(project(":be-core"))
+	implementation(libs.spring.cloud.starter.config)
+	implementation(libs.spring.cloud.starter.netflix.eureka.client)
 	implementation(libs.spring.boot.starter.web)
 	implementation(libs.spring.boot.starter.security)
 	implementation(libs.spring.boot.starter.oauth2.client)
 	implementation(libs.spring.boot.starter.oauth2.authorization.server)
-	implementation(libs.spring.cloud.starter.config)
 	implementation(libs.springdoc.openapi.starter.webmvc.api)
 }
