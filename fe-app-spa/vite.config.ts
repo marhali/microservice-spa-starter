ff54741e9553e4e984242601ac84cd2a7d5bd0ab
marhali <me@marhali.de>
2024-10-15 20:19:37 +0200

    feat: configure backend proxy

--- fe-app-spa/vite.config.ts
+++ fe-app-spa/vite.config.ts
@@ -6,4 +6,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8089',
+        xfwd: true,
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        xfwd: true,
+      },
+    },
+  },
 });
