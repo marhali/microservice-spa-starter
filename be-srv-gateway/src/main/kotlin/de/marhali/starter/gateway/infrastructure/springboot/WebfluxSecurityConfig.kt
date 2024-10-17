31e3597117c561e5e1dcddcc5b63267f90c6f943
marhali <me@marhali.de>
2024-10-17 22:22:20 +0200

    feat: optimize login and logout flow

--- be-srv-gateway/src/main/kotlin/de/marhali/starter/gateway/infrastructure/springboot/WebfluxSecurityConfig.kt
+++ be-srv-gateway/src/main/kotlin/de/marhali/starter/gateway/infrastructure/springboot/WebfluxSecurityConfig.kt
@@ -9,32 +9,22 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository
-import org.springframework.security.web.server.csrf.CsrfToken
-import org.springframework.web.server.WebFilter
-import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
 class WebfluxSecurityConfig(
-	private val clientRegistrationRepository: ReactiveClientRegistrationRepository
+	private val clientRegistrationRepository: ReactiveClientRegistrationRepository,
 ) {
 	@Bean
 	fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-		val cookieServerCsrfTokenRepository = CookieServerCsrfTokenRepository.withHttpOnlyFalse()
-		cookieServerCsrfTokenRepository.setCookiePath("/")
-
 		http
+			.csrf { csrf -> csrf.disable() }
 			.authorizeExchange { authorize ->
 				authorize.anyExchange().authenticated()
 			}
-			.csrf { csrf ->
-				csrf.csrfTokenRepository(cookieServerCsrfTokenRepository)
-				csrf.csrfTokenRequestHandler(SpaServerCsrfTokenRequestHandler())
-			}
 			.oauth2Login { oauth2 ->
 				// https://github.com/spring-projects/spring-security/issues/8967
-				oauth2.authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/"))
+				oauth2.authenticationSuccessHandler(RedirectServerAuthenticationSuccessHandler("/.."))
 			}
 			.logout { logout ->
 				logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
@@ -43,21 +33,14 @@ class WebfluxSecurityConfig(
 		return http.build()
 	}
 
-	@Bean
-	fun csrfCookieWebFilter(): WebFilter {
-		return WebFilter { exchange, chain ->
-			exchange.getAttributeOrDefault<Mono<Any>>(CsrfToken::class.java.getName(), Mono.empty()).subscribe()
-			chain.filter(exchange)
-		}
-	}
-
 	private fun oidcLogoutSuccessHandler(): ServerLogoutSuccessHandler {
 		val oidcLogoutSuccessHandler =
 			OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository)
 
 		// Sets the location that the End-User's User Agent will be redirected to
 		// after the logout has been performed at the Provider
-		oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/..")
+		// To support both backend and frontend logout we are using a workaround to reconstruct the correct origin
+		oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseScheme}://{baseHost}{basePort}")
 
 		return oidcLogoutSuccessHandler
 	}
